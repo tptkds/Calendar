0d9e7cd5e30beb834fd8460817e3a989e9d09640
peacez <tptkdsd@naver.com>
2023-04-13 18:21:47 +0900

    nex,prev,now Btn 기능 개발 완료

--- script.js
+++ script.js
@@ -1,116 +1,105 @@
-const display_mom = document.querySelector(".display_mom");
+const year = document.querySelector('.year');
+const md = document.querySelector('.md');
+const weekday = document.querySelector('.weekday');
+const calendar_display = document.querySelector('.calendar_display');
 const prevBtn = document.querySelector(".prevBtn");
 const nextBtn = document.querySelector('.nextBtn');
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth();
-let firstDay = new Date(year, month, 1);
-let lastDay = new Date(year, month + 1, 0);
-const nowDay = date.getDay();
+const nowBtn = document.querySelector(".nowBtn");
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+/*const leapYear = [31,29,31,30,31,30,31,31,30,31,30,31];
+const notLeapYear = [31,28,31,30,31,30,31,31,30,31,30,31];*/
+const week = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const dateNow = new Date();
+const yearNow = dateNow.getFullYear();
+let month = dateNow.getMonth();
+console.log(month);
 
-let nowMonth = date.getMonth();
+function setDate(y, m, d, wd) { 
+  year.innerText = y;
 
-prevBtn.addEventListener("click", getPrevDate);
-
-function getNowDate() {
-  displayDate(year, month, firstDay, lastDay);
-}
-
-function getPrevDate() {
-  nowMonth -= 1;
-  firstDay = new Date(year, nowMonth, 1);
-  lastDay = new Date(year, nowMonth + 1, 0);
-  document.querySelector('.display').remove();
-  displayDate(year, nowMonth, firstDay, lastDay);
-}
-
-function displayDate(y, m, fd, ld) {
-  let n = 0;
-  const display = document.createElement('div');
-  display.className = 'display';
-  display_mom.appendChild(display);
-  for (let i = 0; i < 6; i++) {
-    // 6번째 라인까지 나타낸다, 첫번째라인과 마지막라인은 저번달 및 다음달의 날짜를 같이 표시한다.
-    const ul = document.createElement("ul");
-    if (i == 0) {
-      //첫번째 ul인 경우
-      const prevMonth_lastDay = new Date(y, m, 0);
-      if (fd.getDay() == 0) {
-        // 첫째주가 일요일부터 시작할 때 => 저번달의 마지막주 7일치를 첫번째 ul에 표시해야 한다.
-        for (let j = prevMonth_lastDay - 6; j <= prevMonth_lastDay; j++) {
-          const li = document.createElement("li");
-          li.innerText = j;
-          li.className = 'notNow';
-          ul.appendChild(li);
-        }
-      } else {
-        // 첫째주가 월요일(1) 이상으로 시작할 때 => 저번달의 일부분만 ul에 표시해야 한다.
-        for (
-          // 저번달 마지막주 첫번째 ul에 추가
-          let j = prevMonth_lastDay.getDate() - fd.getDay() + 1;
-          j <= prevMonth_lastDay.getDate();
-          j++
-        ) {
-          const li = document.createElement("li");
-          li.innerText = j;
-          li.className = 'notNow';
-          ul.appendChild(li);
-        }
-        for (let j = fd.getDay(); j < 7; j++) {
-          n += 1;
-          const li = document.createElement("li");
-          li.innerText = n;
-          ul.appendChild(li);
-        }
-      }
-    } else {
-        for (let j = 0; j < 7; j++) {
-        n += 1;
-        const li = document.createElement("li");
-        li.innerText = n;
-        ul.appendChild(li);
-        if (n == ld.getDate()) {
-          // n이 마지막날에 도달했다면 => 이제부터는 다음달 날짜가 표시된다.
-          console.log(n, ul)
-          if (i == 4) {
-            //5번째라인에서 마지막날에 도달했다면 남은 5번째라인의 빈공간 및 6번째라인을 다음달 날짜로 채운다.
-            let nextD = 0; //다음달 날짜시작
-            console.log(n, ul)
-            for (let k = j + 1; k < 7; k++) {
-              console.log(n, ul)
-              console.log(k)
-              nextD++;
-              const li = document.createElement("li");
-              li.className = 'notNow';
-              li.innerText = nextD;
-              ul.appendChild(li);
-            }
-            const ul = document.createElement("ul"); //6번째 ul
-            for (let k = 0; k < 7; k++) {
-              nextD++;
-              const li = document.createElement("li");
-              li.className = 'notNow';
-              li.innerText = nextD;
-              ul.appendChild(li);
-            }
-          } else {
-            //6번쨰라인에서 마지막날에 도달했다면 남은 6번째라인의 빈공간을 다음달 날짜로 채운다.
-            let nextD = 0; //다음달 날짜시작
-            for (let k = j + 1; k < 7; k++) {
-              nextD++;
-              const li = document.createElement("li");
-              li.className = 'notNow';
-              li.innerText = nextD;
-              ul.appendChild(li);
-            }
-          }
-          break;
-        }
-      }
-    }
-    display.appendChild(ul);
-    if (n == ld.getDate()) break;
+  if(d == "x" && wd == "x") {
+    md.innerText = months[m];
+    weekday.innerText = "";
+  }
+  
+  else {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+    md.innerText = months[m] + " " + d + "th";
+    weekday.innerText = week[wd];
   }
 }
 
-getNowDate();
+function setCalendar(y, m) {
+  let firstDay = new Date(y, m, 1);
+  let lastDay = new Date (y, m + 1, 0);
+  let lastMonth_lastDay = new Date(y, m, 0);
+  let n = 0;
+  let nextMonth_n = 0;
+    
+  for(let i = 0; i < 6; i++) {
+    let ul = document.createElement('ul');
+    ul.className = 'calendar_display_ul'
+    calendar_display.appendChild(ul);
+
+    for(let j = 0; j < 7; j++) {
+      let li = document.createElement('li');
+      
+      if(i == 0 && j < firstDay.getDay() ) {
+        li.innerText = new Date(lastMonth_lastDay.getFullYear(), lastMonth_lastDay.getMonth(), lastMonth_lastDay.getDate() - (firstDay.getDay() - j + 1)).getDate();
+        li.className = 'notNow';
+      }
+    
+      else if(n == lastDay.getDate()) {
+        nextMonth_n++;
+        li.innerText = nextMonth_n;
+        li.className = 'notNow';
+      }
+
+      else {
+        n++;
+        li.innerText = n;
+      }
+        
+      calendar_display.lastChild.appendChild(li);
+    }
+  }
+}
+
+prevBtn.addEventListener("click", clickPrevBtnHandler);
+nextBtn.addEventListener("click", clickNextBtnHandler);
+nowBtn.addEventListener("click", clickNowBtnHandler);
+
+function clickPrevBtnHandler() {
+  removeUl();
+  setCalendar(yearNow, --month);
+
+  let prevDate = new Date(yearNow, month, 1);
+
+  if(prevDate.getFullYear() == dateNow.getFullYear() && prevDate.getMonth() == dateNow.getMonth()) setDate(yearNow, month, dateNow.getDate(),dateNow.getDay());
+  else setDate(prevDate.getFullYear(), prevDate.getMonth(), "x", "x");
+}
+
+function clickNextBtnHandler() {
+  removeUl();
+  setCalendar(yearNow, ++month);
+
+  let nextDate = new Date(yearNow, month, 1);
+
+  if(nextDate.getFullYear() == dateNow.getFullYear() && nextDate.getMonth() == dateNow.getMonth()) setDate(yearNow, month, dateNow.getDate(),dateNow.getDay());
+  else setDate(nextDate.getFullYear(), nextDate.getMonth(), "x", "x");
+}
+
+function clickNowBtnHandler() {
+  removeUl();
+
+  month = dateNow.getMonth();
+  
+  setDate(yearNow, month, dateNow.getDate(),dateNow.getDay());
+  setCalendar(yearNow, month);
+}
+
+function removeUl() {
+  document.querySelectorAll('.calendar_display_ul').forEach(element => element.remove());
+}
+
+setDate(yearNow, month, dateNow.getDate(),dateNow.getDay());
+setCalendar(yearNow, month);
